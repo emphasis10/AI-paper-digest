806c9f91f9d982006c8d3d35d4358fcc74bde5a6
emphasis10 <calmlamb@naver.com>
2024-04-11 22:53:12 +0900

    Clean up directory

--- gen_list.py
+++ gen_list.py
@@ -3,11 +3,12 @@ import os
 with open('README.md', 'w') as wf:
     wf.write('# Paper List\n')
     ym = None
-    for i in sorted(os.listdir(), reverse=True):
+    for i in sorted(os.listdir('summaries'), reverse=True):
+        i = os.path.join('summaries', i)
         if i == 'README.md' or i.endswith('.py') or os.path.isdir(i):
             continue
-        if not ym or ym != i.split('.')[0]:
-            ym = i.split('.')[0]
+        if not ym or ym != i.split('/')[-1].split('.')[0]:
+            ym = i.split('/')[-1].split('.')[0]
             wf.write(f'## {ym}\n')
 
         with open(i, 'r') as f2:
